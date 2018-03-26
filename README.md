cf03ce188de60cac87ac8b03962406a15df01ec0
Kevin Mershon <kevin@mershonenterprises.com>
2018-03-26 11:09:26 -0700

    Implement deployment logic, update README

--- README.md
+++ README.md
@@ -11,7 +11,27 @@ Installation
 
 Developing
 --
-`gulp watch-tests`
+Run the following while you are developing to continuously execute tests.
+
+  `gulp watch-tests`
+
+Configuration
+--
+In order to deploy to NetSuite, you will need to configure your account
+credentials in `credentials.json`, and your user must be an Administrator of
+the NetSuite account you are attempting to deploy to.
+
+1. Copy or rename `credentials-sample.json` to `credentials.json`
+2. Go to `Setup > Integration > Web Services Preferences` in your NetSuite account.
+3. Copy the `ACCOUNT ID` value to your `credentials.json` file under `account`,
+  and fill in all other appropriate values for `role`, `email`, and `password`.
+4. Add your user account as an authorized user of the Web Services API, similar to this:
+
+  ![Web Services Configuration](blob/master/doc/web_services_config_example.png)
+
+Deploying
+--
+Run the command `gulp deploy-sandbox` to deploy to your Sandbox account.
 
 License
 --
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
-var gulp  = require('gulp');
-var mocha = require('gulp-mocha');
-var log   = require('gulplog');
+// Testing
+const gulp  = require('gulp');
+const mocha = require('gulp-mocha');
+const log   = require('gulplog');
 
 gulp.task('mocha', function() {
   return gulp.src(['test/**/*.js'], { read: false })
@@ -14,3 +15,63 @@ const watchMocha = function() {
   gulp.watch(['lib/**', 'src/**', 'test/**'], gulp.series('mocha'));
 }
 gulp.task('watch-tests', watchMocha);
+
+
+
+
+
+// NetSuite build tasks
+const uploadToNetsuite = require('netsuite-deploy/lib/index.js');
+const netsuiteSettings = require('./package.json').netsuite;
+const credentials      = require('./credentials');
+
+const keepass          = require('keepass-http-client');
+const merge            = require('merge-stream');
+const gulpUtil         = require('gulp-util');
+const changed          = require('gulp-changed');
+const filenames        = require('gulp-filenames');
+const del              = require('del');
+
+gulp.task('clean', () => {
+  return del('dist/');
+});
+gulp.task('build', () => {
+  const scriptDest = `dist/FileCabinet/${netsuiteSettings.folder}/`;
+  filenames.forget('all');
+  return merge(
+    gulp
+    .src('src/js/**/*.js')
+    .pipe(changed(scriptDest))
+    .pipe(gulp.dest(scriptDest))
+    .pipe(filenames('files'))
+  );
+});
+
+function deploy(environment) {
+  // Even if there are no results, will still be an empty 'all'
+  if (Object.keys(filenames.get('all')).length === 0) {
+    gulpUtil.log(gulpUtil.colors.red('No files to deploy!'));
+    return;
+  }
+
+  let config = filenames.get('objects').length
+    ? Object.assign(credentials, { environment, method: 'sdf', file: 'dist\\' })
+    : Object.assign(credentials, {
+        environment,
+        method: 'suitetalk',
+        file: filenames.get('files', 'full'),
+        path: netsuiteSettings.folder,
+        base: `${__dirname}/dist/FileCabinet/${netsuiteSettings.folder}/`,
+      });
+
+  return uploadToNetsuite(config);
+}
+
+gulp.task('deploy-sandbox',
+  gulp.series(
+    ['build'],
+    () => {
+      return deploy('sandbox');
+    }
+  )
+);
